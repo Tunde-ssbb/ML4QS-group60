2b85bead3fd441adc600b215d0f985514ea63e41
Aron-VH <aronvh6@gmail.com>
2024-06-10 14:07:21 +0200

    rest

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 #%%
 from src.preprocessing.outlier_removal import OutlierRemoval
-def main():
+import src.visualisation.data_visualisation as vis
+
+def do_outlier_removal():
 	orem = OutlierRemoval()
 	orem.read_data()
 	orem.remove_outliers()
-	orem.visualize_data(after_removal=True)
+	vis.create_distribution_plot(orem.combined_data, 'orem', './figures/without_outliers')
+
+def main():
+	do_outlier_removal()
 
 main()
+
+
 
 # %%
--- src/preprocessing/outlier_removal.py
+++ src/preprocessing/outlier_removal.py
@@ -1,132 +1,46 @@
-""" 
-Maybe also do different techniques depending on associated expertise?
-- distance: k-distance neighbourhood as there is a steady increase, so any drastic changes should be caught, but there is not e.g. a simple normal distribution
-- pace(s): k-distribution
-- HPM: k-distance neighbourhood to catch big jumps
-- arm/leg acceleration: k-distance for the y-axis, simple distribution for the others
-- arm/leg rotation: k-distance, due to wide variety of values
-"""
 #%% Dependencies
 # Externals
 import pandas as pd 
 import os
-import matplotlib.pyplot as plt
-import numpy as np
 # Internals
 from lib.util.VisualizeDataset import VisualizeDataset as vd
 from lib.Chapter3.OutlierDetection import DistanceBasedOutlierDetection, DistributionBasedOutlierDetection
 
 class OutlierRemoval():
 	def __init__(self) -> None:
-		self.data_files:str[pd.DataFrame] = {}
 		self.combined_data = pd.DataFrame()
 	
 	def read_data(self):
 		path = os.path.abspath(os.path.join(os.getcwd(), 'measurement-data/full_data.csv'))
 		self.combined_data = pd.read_csv(path)
 
-		""" print(os.getcwd())
-		walk_dir = os.path.abspath(os.path.join(os.getcwd(), 'measurement-data/processed/'))
-		print(walk_dir)
-		for root, subdirs, files in os.walk(walk_dir):
-			print('--\nroot = ' + root)
-			for subdir in subdirs:
-				print('\t- subdirectory ' + subdir)
-				with os.scandir(root+'/'+subdir) as it:
-					for entry in it:
-						data = pd.read_csv(entry)
-						self.data_files[subdir+'-'+entry.name] = data """
-	
-	def visualize_data(self, after_removal: bool):
-		for file, data in self.data_files.items():
-			vis = vd(file)
-			name = file.split('-')[0]
-			session = file.rsplit('_')[-1][0]
-			moment = 'after-outlier-removal' if after_removal else 'before-outlier-removal'
+	def _remove_outliers_distribution_based_mixed_models(self, col):
+		print(f'#rows prior to outlier removal of col {col}: {self.combined_data.shape[0]}')
+		# Applying the k normal distributions fitting
+		mm = DistributionBasedOutlierDetection()
+		with_mixture = mm.mixture_model(self.combined_data, col)
 
-			# Movement figs
+		# Constants
+		cutoff = 0.05
 
-			# Create subplots
-			fig, axs = plt.subplots(4, 1, figsize=(12, 16), sharex=True)
-
-			# First plot: arm acceleration
-			axs[0].set_title(name+'_'+session+'_movement_'+moment)
-			axs[0].plot(data['Time'], data[name+'_Arm_'+session+'_X (m/s^2)'], "r+", label='x')
-			axs[0].plot(data['Time'], data[name+'_Arm_'+session+'_Y (m/s^2)'], "b+", label='y')
-			axs[0].plot(data['Time'], data[name+'_Arm_'+session+'_Z (m/s^2)'], "g+", label='z')
-			axs[0].set_ylabel("Arm acceleration (m/s^2)")
-			axs[0].legend(loc = 'upper left')
-
-			# Second plot: arm rotation
-			axs[1].plot(data['Time'], data[name+'_Arm_'+session+'_X (rad/s)'], "r+", label='x')
-			axs[1].plot(data['Time'], data[name+'_Arm_'+session+'_Y (rad/s)'], "b+", label='y')
-			axs[1].plot(data['Time'], data[name+'_Arm_'+session+'_Z (rad/s)'], "g+", label='z')
-			axs[1].set_ylabel("Arm rotation (rad/s)")
-			axs[1].legend(loc = 'upper left')
-
-			# Third plot: leg acceleration
-			axs[2].plot(data['Time'], data[name+'_Been_'+session+'_X (m/s^2)'], "r+", label='x')
-			axs[2].plot(data['Time'], data[name+'_Been_'+session+'_X (m/s^2)'], "b+", label='y')
-			axs[2].plot(data['Time'], data[name+'_Been_'+session+'_X (m/s^2)'], "g+", label='z')
-			axs[2].set_ylabel("Leg acceleration (m/s^2)")
-			axs[2].legend(loc = 'upper left')
-
-			# Fourth plot: leg rotation
-			axs[3].plot(data['Time'], data[name+'_Been_'+session+'_X (rad/s)'], "r+", label='x')
-			axs[3].plot(data['Time'], data[name+'_Been_'+session+'_X (rad/s)'], "b+", label='y')
-			axs[3].plot(data['Time'], data[name+'_Been_'+session+'_X (rad/s)'], "g+", label='z')
-			axs[3].set_ylabel("Leg rotation (rad/s)")
-			axs[3].legend(loc = 'upper left')
-
-			# Set common x-label
-			axs[3].set_xlabel("Time (min)")
-
-			# Set x-ticks only for the lowest subplot
-			for ax in axs[:-1]:
-				ax.tick_params(labelbottom=False)
-			
-			# Format the x-ticks as integers representing minutes
-			axs[3].set_xticks(np.arange(0,3001, 600))
-			axs[3].set_xticklabels(range(0,6))
-
-			# Saving fig
-			img_file=name+'_'+session+'_movement_'+moment+'.png'
-			subdir = file.split('.')[0]
-			plt.savefig("./figures/" + subdir + '/' + img_file)
-
-			# Performance figs
-			fig, axs = plt.subplots(3, 1, figsize=(12, 6), sharex=True)
-
-			axs[0].set_title(name+'_'+session+'_performance_'+moment)
-			axs[0].plot(data['Time'], data[name+'_'+session+'_Distance (meters)'], 'r.')
-			axs[0].set_ylabel("Distance (meters)")
-
-			axs[1].plot(data['Time'], data[name+'_'+session+'_Pace (seconds)'], 'b.')
-			axs[1].set_ylabel("Pace (seconds)")
-
-			axs[2].plot(data['Time'], data[name+'_Arm_'+session+'_HR (bpm)'], 'g.')
-			axs[2].set_ylabel("heartrate (bpm)")
-
-			axs[2].set_xlabel("Time (min)")
-
-			for ax in axs[:-1]:
-				ax.tick_params(labelbottom=False)
-
-			# Format the x-ticks as integers representing minutes
-			axs[2].set_xticks(np.arange(0,3001, 600))
-			axs[2].set_xticklabels(range(0,6))
-
-			# Saving fig
-			img_file=name+'_'+session+'_performance_'+moment+'.png'
-			plt.savefig("./figures/" + subdir + '/' + img_file)
-
-	def _remove_outliers_distribution_based_mixed_models(self, col, k):
-		pass
+		# Removing outliers
+		mask = with_mixture[col+'_mixture'] <= cutoff
+		self.combined_data = self.combined_data[mask]
+		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
 
 	def _remove_outliers_distribution_based_chauvenet(self, col):
-		pass
+		print(f'#rows prior to outlier removal of col {col}: {self.combined_data.shape[0]}')
+		# Applying chauvenet's criterion
+		ch = DistributionBasedOutlierDetection()
+		with_outlier = ch.chauvenet(self.combined_data, col, 2)
+
+		# Removing outliers
+		mask = with_outlier[col+'_outlier'] == 0
+		self.combined_data = self.combined_data[mask]
+		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
 
 	def _remove_outliers_density_based(self, col):
+		print(f'#rows prior to outlier removal of col {col}: {self.combined_data.shape[0]}')
 		# Calculating LOFs
 		od = DistanceBasedOutlierDetection()
 		with_lof = od.local_outlier_factor(self.combined_data, [col], 'euclidean', 5)
@@ -138,10 +52,14 @@ class OutlierRemoval():
 		# Removing outliers based on cutoff
 		mask = with_lof['lof'] >= cutoff_low & with_lof['lof'] <= cutoff_high
 		self.combined_data = self.combined_data[mask]
+		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
 
 	def remove_outliers(self):
+		print(self.combined_data.shape[0])
 		for attribute in self.combined_data.columns.values:
-			if 'dist' in attribute:
+			if '0' in attribute or 'time' in attribute or 'experience_level' in attribute or 'session_id' in attribute:
+				continue
+			elif 'dist' in attribute:
 				self._remove_outliers_density_based(attribute)
 			elif 'pace' in attribute:
 				self._remove_outliers_density_based(attribute)
@@ -150,19 +68,20 @@ class OutlierRemoval():
 			elif 'leg_gyr' and 'z' in attribute:
 				self._remove_outliers_density_based(attribute)
 			elif 'leg_gyr' in attribute:
-				self._remove_outliers_distribution_based(attribute)
-			elif 'arm_gyr' in attribute:
-				self._remove_outliers_distribution_based(attribute)
+				self._remove_outliers_distribution_based_mixed_models(attribute)
+			#elif 'arm_gyr' in attribute:
+			#	self._remove_outliers_distribution_based_chauvenet(attribute)
 			elif 'leg_acc' and 'x' in attribute:
-				self._remove_outliers_distribution_based(attribute)
+				self._remove_outliers_distribution_based_chauvenet(attribute)
 			elif 'leg_acc' and 'y' in attribute:
 				self._remove_outliers_density_based(attribute)
 			elif 'leg_acc' and 'z' in attribute:
-				self._remove_outliers_distribution_based(attribute)
+				self._remove_outliers_distribution_based_mixed_models(attribute)
 			elif 'arm_acc' in attribute:
-				self._remove_outliers_distribution_based(attribute)
+				self._remove_outliers_distribution_based_mixed_models(attribute)
 			else:
 				raise ValueError(f'Attribute {attribute} was not expected.')
 		
 	def output_data(self):
+		self.combined_data.to_csv('./measurement-data/outliers_removed/orem.csv')
 		pass
