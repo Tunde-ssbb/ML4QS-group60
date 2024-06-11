2c538177a668b52eef22c07570cd35dcaecc804e
Aron-VH <aronvh6@gmail.com>
2024-06-11 11:47:59 +0200

    desperate measures

--- src/preprocessing/outlier_removal.py
+++ src/preprocessing/outlier_removal.py
@@ -2,6 +2,7 @@
 # Externals
 import pandas as pd 
 import os
+import numpy as np
 # Internals
 from lib.util.VisualizeDataset import VisualizeDataset as vd
 from lib.Chapter3.OutlierDetection import DistanceBasedOutlierDetection, DistributionBasedOutlierDetection
@@ -21,16 +22,17 @@ class OutlierRemoval():
 		with_mixture = mm.mixture_model(self.combined_data, col)
 
 		# Constants
-		cutoff = 0.05
+		cutoff = 0.2
 
 		# Removing outliers
 		mask = with_mixture[col+'_mixture'] <= cutoff
-		self.combined_data = with_mixture[mask]
-		self.combined_data.drop(col+'_mixture', axis=1, inplace=True)
+		with_mixture = with_mixture[mask]
+		self.combined_data = with_mixture.drop(col+'_mixture', axis=1)
 		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
 
 	def _remove_outliers_distribution_based_chauvenet(self, col):
 		print(f'(chauvenet) #rows prior to outlier removal of col {col}: {self.combined_data.shape[0]}')
+		print(np.isnan(self.combined_data[col]).any())
 		# Applying chauvenet's criterion
 		ch = DistributionBasedOutlierDetection()
 #		self.combined_data = self.combined_data[self.combined_data[col].notna()]
@@ -39,7 +41,7 @@ class OutlierRemoval():
 		# Removing outliers
 		mask = ~with_outlier[col+'_outlier']
 		self.combined_data = with_outlier[mask]
-		self.combined_data.drop(col+'_outlier', axis=1, inplace=True)
+		self.combined_data = self.combined_data.drop(col+'_outlier', axis=1)
 		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
 
 	def _remove_outliers_density_based(self, col):
@@ -54,30 +56,26 @@ class OutlierRemoval():
 
 		# Removing outliers based on cutoff
 		mask = (with_lof['lof'] >= cutoff_low) & (with_lof['lof'] <= cutoff_high)
-		self.combined_data = with_lof[mask]
-		self.combined_data.drop('lof', axis=1, inplace=True)
+		with_lof = with_lof[mask]
+		self.combined_data = with_lof.drop('lof', axis=1)
 		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
 
 	def remove_outliers(self):
 		print(self.combined_data.shape[0])
 		for attribute in self.combined_data.columns.values:
 			print(attribute)
-			if '0' in attribute or 'time' in attribute or 'experience_level' in attribute or 'session_id' in attribute:
+			if 'dist' in attribute or 'pace' in attribute or '0' in attribute or 'time' in attribute or 'experience_level' in attribute or 'session_id' in attribute:
 				continue
-			elif 'dist' in attribute:
-				self._remove_outliers_density_based(attribute)
-			elif 'pace' in attribute:
-				self._remove_outliers_density_based(attribute)
 			elif 'HR' in attribute:
 				self._remove_outliers_density_based(attribute)
 			elif 'leg_gyr' in attribute and 'z' in attribute:
 				self._remove_outliers_density_based(attribute)
 			elif 'leg_gyr' in attribute:
 				self._remove_outliers_distribution_based_mixed_models(attribute)
-			#elif 'arm_gyr' in attribute:
-		#		self._remove_outliers_distribution_based_chauvenet(attribute)
+			elif 'arm_gyr' in attribute:
+				self._remove_outliers_distribution_based_mixed_models(attribute)
 			elif 'leg_acc' in attribute and 'x' in attribute:
-				self._remove_outliers_distribution_based_chauvenet(attribute)
+				self._remove_outliers_distribution_based_mixed_models(attribute)
 			elif 'leg_acc' in attribute and 'y' in attribute:
 				self._remove_outliers_density_based(attribute)
 			elif 'leg_acc' in attribute and 'z' in attribute:
