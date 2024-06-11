2a29ecdbbb0e1cdcfe462dfecf46c1991e8ec24a
Aron-VH <aronvh6@gmail.com>
2024-06-11 15:03:14 +0200

    plotjes

--- src/preprocessing/outlier_removal.py
+++ src/preprocessing/outlier_removal.py
@@ -3,6 +3,7 @@
 import pandas as pd 
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 # Internals
 from lib.util.VisualizeDataset import VisualizeDataset as vd
 from lib.Chapter3.OutlierDetection import DistanceBasedOutlierDetection, DistributionBasedOutlierDetection
@@ -26,13 +27,15 @@ class OutlierRemoval():
 		mean = with_mixture[col+'_mixture'].mean()
 		print(with_mixture[col+'_mixture'].std())
 		print(with_mixture[col+'_mixture'].mean())
-		cutoff = mean + 2 * std
+		cutoff = mean - 1.5 * std
 
 		# Removing outliers
-		mask = with_mixture[col+'_mixture'] <= cutoff
-		with_mixture = with_mixture[mask]
+		# TODO: I do not fully understand what the values returnt mean
+		with_mixture[col].mask(with_mixture[col+'_mixture'] <= cutoff, inplace=True)
+		self.outlier_visualization(col+'_mixture', with_mixture)
+		
 		self.combined_data = with_mixture.drop(col+'_mixture', axis=1)
-		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
+		print(f'#NaNs: {self.combined_data.isna().sum()}')
 
 	def _remove_outliers_density_based(self, col):
 		print(f'(LOF) #rows prior to outlier removal of col {col}: {self.combined_data.shape[0]}')
@@ -41,18 +44,19 @@ class OutlierRemoval():
 		with_lof = od.local_outlier_factor(self.combined_data, [col], 'euclidean', 5)
 
 		# Constants
-		std = with_lof[col+'_mixture'].std()
-		mean = with_lof[col+'_mixture'].mean()
-		print(with_lof[col+'_mixture'].std())
-		print(with_lof[col+'_mixture'].mean())
+		std = with_lof['lof'].std()
+		mean = with_lof['lof'].mean()
+		print(with_lof['lof'].std())
+		print(with_lof['lof'].mean())
 		cutoff_low = mean - std
 		cutoff_high = mean + std
 		
 		# Removing outliers based on cutoff
-		mask = (with_lof['lof'] >= cutoff_low) & (with_lof['lof'] <= cutoff_high)
-		with_lof = with_lof[mask]
+		with_lof[col].mask((with_lof['lof'] <= cutoff_low) | (with_lof['lof'] >= cutoff_high), inplace=True)
+		self.outlier_visualization('lof')
+		
 		self.combined_data = with_lof.drop('lof', axis=1)
-		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
+		print(f'#NaNs: {self.combined_data.isna().sum()}')
 
 	def remove_outliers(self):
 		print(self.combined_data.shape[0])
@@ -83,3 +87,22 @@ class OutlierRemoval():
 		
 	def write_data(self):
 		self.combined_data.to_csv('./measurement-data/outliers_removed/orem.csv')
+
+	def outlier_visualization(self, col, data, path='./figures/without_outliers'):
+		# Create subplots
+		fig, axs = plt.subplots(figsize=(16, 16), squeeze=False)
+
+		fig.suptitle(col, fontsize = 20)
+
+		# First plot: arm acceleration
+		axs[0,0].set_title(col, size = 20)
+		axs[0,0].hist(data[col], bins = 100, alpha = 0.5, label=col[0])
+		axs[0,0].set_xlabel("outlier method", fontsize=20)
+		axs[0,0].set_ylabel("frequency", fontsize=20)
+		axs[0,0].tick_params(labelsize=16)
+		axs[0,0].legend(loc = 'upper left', fontsize = 20)
+
+		p = path + "/" +  col + "_distributions"
+		plt.savefig(p)
+
+		print(f"Saved distribution plot to: {p}")
