fd0ab7deccdda4372eb8f0a37e8d6715117302ae
Aron-VH <aronvh6@gmail.com>
2024-06-11 12:57:10 +0200

    Better cutoffs

--- src/preprocessing/outlier_removal.py
+++ src/preprocessing/outlier_removal.py
@@ -15,33 +15,23 @@ class OutlierRemoval():
 		path = os.path.abspath(os.path.join(os.getcwd(), 'measurement-data/full_data.csv'))
 		self.combined_data = pd.read_csv(path)
 
-	def _remove_outliers_distribution_based_mixed_models(self, col):
+	def _remove_outliers_distribution_based_mixed_models(self, col, n_components):
 		print(f'(mixed models) #rows prior to outlier removal of col {col}: {self.combined_data.shape[0]}')
 		# Applying the k normal distributions fitting
 		mm = DistributionBasedOutlierDetection()
-		with_mixture = mm.mixture_model(self.combined_data, col)
+		with_mixture = mm.mixture_model(self.combined_data, col, n_components)
 
 		# Constants
-		cutoff = 0.2
+		std = with_mixture[col+'_mixture'].std()
+		mean = with_mixture[col+'_mixture'].mean()
+		print(with_mixture[col+'_mixture'].std())
+		print(with_mixture[col+'_mixture'].mean())
+		cutoff = mean + 2 * std
 
 		# Removing outliers
 		mask = with_mixture[col+'_mixture'] <= cutoff
 		with_mixture = with_mixture[mask]
 		self.combined_data = with_mixture.drop(col+'_mixture', axis=1)
-		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
-
-	def _remove_outliers_distribution_based_chauvenet(self, col):
-		print(f'(chauvenet) #rows prior to outlier removal of col {col}: {self.combined_data.shape[0]}')
-		print(np.isnan(self.combined_data[col]).any())
-		# Applying chauvenet's criterion
-		ch = DistributionBasedOutlierDetection()
-#		self.combined_data = self.combined_data[self.combined_data[col].notna()]
-		with_outlier = ch.chauvenet(self.combined_data, col, 2)
-
-		# Removing outliers
-		mask = ~with_outlier[col+'_outlier']
-		self.combined_data = with_outlier[mask]
-		self.combined_data = self.combined_data.drop(col+'_outlier', axis=1)
 		print(f'#rows after outlier removal of col: {col}: {self.combined_data.shape[0]}')
 
 	def _remove_outliers_density_based(self, col):
@@ -51,9 +41,13 @@ class OutlierRemoval():
 		with_lof = od.local_outlier_factor(self.combined_data, [col], 'euclidean', 5)
 
 		# Constants
-		cutoff_low = 0.95
-		cutoff_high = 1.05
-
+		std = with_lof[col+'_mixture'].std()
+		mean = with_lof[col+'_mixture'].mean()
+		print(with_lof[col+'_mixture'].std())
+		print(with_lof[col+'_mixture'].mean())
+		cutoff_low = mean - std
+		cutoff_high = mean + std
+		
 		# Removing outliers based on cutoff
 		mask = (with_lof['lof'] >= cutoff_low) & (with_lof['lof'] <= cutoff_high)
 		with_lof = with_lof[mask]
@@ -71,19 +65,21 @@ class OutlierRemoval():
 			elif 'leg_gyr' in attribute and 'z' in attribute:
 				self._remove_outliers_density_based(attribute)
 			elif 'leg_gyr' in attribute:
-				self._remove_outliers_distribution_based_mixed_models(attribute)
+				self._remove_outliers_distribution_based_mixed_models(attribute, 2)
 			elif 'arm_gyr' in attribute:
-				self._remove_outliers_distribution_based_mixed_models(attribute)
+				self._remove_outliers_distribution_based_mixed_models(attribute, 1)
 			elif 'leg_acc' in attribute and 'x' in attribute:
-				self._remove_outliers_distribution_based_mixed_models(attribute)
+				self._remove_outliers_distribution_based_mixed_models(attribute, 1)
 			elif 'leg_acc' in attribute and 'y' in attribute:
 				self._remove_outliers_density_based(attribute)
 			elif 'leg_acc' in attribute and 'z' in attribute:
-				self._remove_outliers_distribution_based_mixed_models(attribute)
+				self._remove_outliers_distribution_based_mixed_models(attribute, 2)
+			elif 'arm_acc' in attribute and 'x' in attribute:
+				self._remove_outliers_distribution_based_mixed_models(attribute, 2)
 			elif 'arm_acc' in attribute:
-				self._remove_outliers_distribution_based_mixed_models(attribute)
-			#else:
-				#raise ValueError(f'Attribute {attribute} was not expected.')
+				self._remove_outliers_distribution_based_mixed_models(attribute, 1)
+			else:
+				raise ValueError(f'Attribute {attribute} was not expected.')
 		
 	def write_data(self):
 		self.combined_data.to_csv('./measurement-data/outliers_removed/orem.csv')
