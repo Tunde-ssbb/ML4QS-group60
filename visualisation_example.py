cafaa1a14edc82d63be7ec701cd35e2fe08c75e6
Tunde de Vries <devriestunde@gmail.com>
2024-06-18 14:05:35 +0200

    updated figures on full data

--- visualisation_example.py
+++ visualisation_example.py
@@ -8,20 +8,38 @@ import os
 
 
 # read data and define a name
-data_path = "./measurement-data/fouried_data.csv"
-data = pd.read_csv(data_path, index_col=0)
+
 
 freq_path = os.path.abspath("./figures/freq_dist_per_lvl")
 fig_path = os.path.abspath("./figures")
+temp_path = os.path.abspath("./figures/temporal_frequency_new")
 
-features = ['arm_gyr_x', 'arm_gyr_y', 'arm_gyr_z', 'arm_acc_x', 'arm_acc_y', 'arm_acc_z', 'leg_gyr_x', 'leg_gyr_y', 'leg_gyr_z', 'leg_acc_x', 'leg_acc_y', 'leg_acc_z']
 
-for feature in features:
-    dv.create_freq_distributions_by_experience_level_plot(data, feature, freq_path)
+data_path = "./measurement-data/full_data.csv"
+data = pd.read_csv(data_path)
 
-dv.create_distribution_plot(data, "full_data",  fig_path)
-dv.create_movement_plot(data, "full_data", fig_path)
+
+
+
+#dv.create_movement_plot(data, "full_data", fig_path)
 dv.create_performance_plot(data, "full_data" , fig_path)
+
+
+# data_path = "./src/machine_learning/fouried_data.csv"
+# data = pd.read_csv(data_path)
+
+# features = ['arm_gyr_x', 'arm_gyr_y', 'arm_gyr_z', 'arm_acc_x', 'arm_acc_y', 'arm_acc_z', 'leg_gyr_x', 'leg_gyr_y', 'leg_gyr_z', 'leg_acc_x', 'leg_acc_y', 'leg_acc_z']
+
+# for feature in features:
+#     dv.create_temporal_frequency_plot(data, feature, temp_path)
+
+
+# for feature in features:
+#     dv.create_freq_distributions_by_experience_level_plot(data, feature, freq_path)
+
+# dv.create_distribution_plot(data, "full_data",  fig_path)
+
+
 
 
 
