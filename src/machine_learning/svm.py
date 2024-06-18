7864a3da1764a41e7dae6fe1986a192fa9fbfb6a
Tunde de Vries <devriestunde@gmail.com>
2024-06-18 14:34:31 +0200

    extra run test session 11

--- src/machine_learning/svm.py
+++ src/machine_learning/svm.py
@@ -93,8 +93,7 @@ X_train, y_train, X_test, y_test = train_test_split_full_session(X, y, session_i
 print(X_test)
 print(y_test)
 
-print(f"test set session id: {session_id}")
-print(f"exp lvl: {y_test.mean()}")
+
 
 
 start = time.time()
@@ -134,6 +133,9 @@ pipeline.fit(X_train, y_train)
 
 # Make predictions on the test data
 y_pred = pipeline.predict(X_test)
+
+print(f"test set session id: {session_id}")
+print(f"exp lvl: {y_test.mean()}")
 
 # Evaluate the model
 for i in range(5):
