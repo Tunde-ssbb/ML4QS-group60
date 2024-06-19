090d3c73156fcadc81269cd5fd5e9b50782b8a9d
Tunde de Vries <devriestunde@gmail.com>
2024-06-19 19:33:26 +0200

    updated lstm and results

--- src/machine_learning/lstm.py
+++ src/machine_learning/lstm.py
@@ -5,8 +5,10 @@ import numpy as np
 import pandas as pd
 import time
 import util
+import tensorflow as tf
 
-def run_LSTM(data, session_id):
+def run_LSTM(data, session_id, random_seed = 42):
+    np.random.seed(random_seed)
 
     target = 'exp_lvl'
 
@@ -22,7 +24,7 @@ def run_LSTM(data, session_id):
     print(y_test)
 
     print(f"test set session id: {session_id}")
-    print(f"exp lvl: {y_test.mean()}")
+    print(f"exp lvl: {np.argmax(y_test.mean())}")
 
     X_train_rs = X_train.values.reshape(1, X_train.shape[0], X_train.shape[1])
 
@@ -47,7 +49,7 @@ def run_LSTM(data, session_id):
 
     #model.summary()
 
-    model.fit(X_train_rs, y_train_rs, batch_size=1, epochs=1)
+    model.fit(X_train_rs, y_train_rs, batch_size=1, epochs=20)
 
     # Evaluate the model
     loss, accuracy = model.evaluate(X_test_rs, y_test_rs)
@@ -81,21 +83,26 @@ def run_LSTM(data, session_id):
 
     print(f"time: {end-start}s")
 
-    return acc
+    return acc, tr, np.argmax(y_test.mean())
 
 
 data = util.read_and_preprocess()
+
 session_ids = data['session_id'].unique()
 
 accuracies = []
+trs = []
+levels = []
 
 for session_id in session_ids:
-    acc = run_LSTM(data, session_id)
+    acc, tr, exp_lvl = run_LSTM(data, session_id)
 
     accuracies.append(acc)
+    trs.append(np.mean(tr))
+    levels.append(exp_lvl)
 
-for session, accuracy in zip(session_ids, accuracies):
-    print(session, accuracy, sep='\t\t')
+for session, accuracy, exp_lvl, tr in zip(session_ids, accuracies, levels, trs):
+    print(session, accuracy, exp_lvl, tr, sep='\t\t')
     
 #accuracy = accuracy_score(ta, tr)
 #print(f'Accuracy: {accuracy:.2f}')
